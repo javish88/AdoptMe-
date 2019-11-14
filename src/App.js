4c5c8aaba5c26b8f6b630d435599e3992ce5518f
Javier <tsakasu88@gmail.com>
2019-11-14 10:57:50 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+// your code will be starting here
+import React, { useState } from "react";
+import "regenerator-runtime/runtime";
+import { render } from "react-dom";
+import { Router, Link } from "@reach/router";
+import SearchParams from "./SearchParams";
+import Details from "./Details";
+import ThemeContext from "./ThemeContext";
+
+const App = () => {
+  const themeHook = useState("darkblue");
+  return (
+    <React.StrictMode>
+      <ThemeContext.Provider value={themeHook}>
+        <div>
+          <header>
+            <Link to="/">Adopt Me!</Link>
+          </header>
+
+          <Router>
+            <SearchParams path="/" />
+            <Details path="/details/:id" />
+          </Router>
+        </div>
+      </ThemeContext.Provider>
+    </React.StrictMode>
+  );
+};
+
+render(<App />, document.getElementById("root"));
